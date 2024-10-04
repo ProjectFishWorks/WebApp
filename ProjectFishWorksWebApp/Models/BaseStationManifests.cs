3fb44ba083144a29f312086a870199bf045e326c
Kayleb814 <kayleb_s@hotmail.com>
2024-10-04 10:06:20 -0700

    change manifest topic

--- ProjectFishWorksWebApp/Models/BaseStationManifests.cs
+++ ProjectFishWorksWebApp/Models/BaseStationManifests.cs
@@ -17,7 +17,7 @@ namespace ProjectFishWorksWebApp.Models
         {
             get
             {
-                var messsages = _mqttService.AllMessages.Where(x => (x.Key.StartsWith($"manifestIn/{_systemID}")));
+                var messsages = _mqttService.AllMessages.Where(x => (x.Key.StartsWith($"manifestOut/{_systemID}")));
 
                 _manifests = new List<BaseStationManifestData>();
 
@@ -47,7 +47,7 @@ namespace ProjectFishWorksWebApp.Models
         {
             string payload = JsonSerializer.Serialize(manifest);
             MqttApplicationMessage message = new MqttApplicationMessageBuilder()
-                .WithTopic($"manifestOut/{manifest.SystemID}/{manifest.BaseStationID}")
+                .WithTopic($"manifestIn/{manifest.SystemID}/{manifest.BaseStationID}")
                 .WithPayload(payload)
                 .WithRetainFlag()
                 .Build();
