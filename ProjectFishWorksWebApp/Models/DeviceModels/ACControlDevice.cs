aea48e766ff824c0a375f682337f2fd313c36287
sssmc <sebastien@robitaille.info>
2024-11-02 16:55:07 -0700

    Add basic page for AC Control

---
+++ ProjectFishWorksWebApp/Models/DeviceModels/ACControlDevice.cs
@@ -0,0 +1,34 @@
+﻿namespace ProjectFishWorksWebApp.Models.DeviceModels
+{
+    public class ACControlDevice : Device
+    {
+        private int nodeID;
+        public ACControlDevice(MQTTnet.ClientLib.MqttService mqttService, int systemID, int basestationID, int nodeID) : base(mqttService, systemID, basestationID)
+        {
+            this.nodeID = nodeID;
+        }
+        public bool Relay1Control
+        {
+            get
+            {
+                return getMessagePayload(nodeID, 2560).data == 1;
+            }
+            set
+            {
+                sendMessageData(nodeID,2560,value ?  (ulong)1 : (ulong)0);
+            }
+        }
+
+        public bool Relay2Control
+        {
+            get
+            {
+                return getMessagePayload(nodeID, 2561).data == 1;
+            }
+            set
+            {
+                sendMessageData(nodeID, 2561, value ? (ulong)1 : (ulong)0);
+            }
+        }
+    }
+}
