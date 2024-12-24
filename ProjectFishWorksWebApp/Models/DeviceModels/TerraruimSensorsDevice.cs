9b195b11a7571b2393b7494cbc97331083d330a8
Sebastien Robitaille <sebastien@robitaille.info>
2024-12-23 23:36:09 -0800

    Add Terraruim Sensors device

---
+++ ProjectFishWorksWebApp/Models/DeviceModels/TerraruimSensorsDevice.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjectFishWorksWebApp.Models.DeviceModels
+{
+    public class TerraruimSensorsDevice : Device
+    {
+        private int nodeID;
+
+        public TerraruimSensorsDevice(MQTTnet.ClientLib.MqttService mqttService, int systemID, int basestationID, int nodeID) : base(mqttService, systemID, basestationID)
+        {
+            this.nodeID = nodeID;
+        }
+
+        public List<HistoryDataRow>? TerraruimSensorsHistory { get; set; }
+
+        public float? Temp
+        {
+            get
+            {
+                return (float?)Math.Round((decimal)getMessagePayload(nodeID, 2560).dataFloat, 2);
+            }
+
+        }
+
+        public float? Humidity
+        {
+            get
+            {
+                return (float?)Math.Round((decimal)getMessagePayload(nodeID, 2561).dataFloat, 2);
+            }
+        }
+
+        public float? SoilTemp
+        {
+            get
+            {
+                return (float?)Math.Round((decimal)getMessagePayload(nodeID, 2562).dataFloat, 2);
+            }
+        }
+
+        public float? SoilMoisture
+        {
+            get
+            {
+                return (float?)Math.Round((decimal)getMessagePayload(nodeID, 2563).dataFloat, 0);
+            }
+        }
+
+    }
+}
